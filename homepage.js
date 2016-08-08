691b9408cda33d7162b5aee533ddf39c778f98c7
Tim Farnam <twfarnam@gmail.com>
2016-08-07 20:00:10 -0400

    added homepage

--- homepage.js
+++ homepage.js
@@ -1,71 +1,81 @@
+(function() {
+  "use strict";
 
-function emailValid(e) {
-  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  var dom = require('dom').default;
+  var log = require('log').default;
 
-  if (re.test($('input.email').value)) {
-    // take off the invalid state
-    if ($('input.email').classList.contains('invalid')) {
-      $('input.email').classList.remove('invalid');
-      $('.email-error').style.visibility = 'hidden';
+  dom('.signup-form').bind('submit', function(e) {
+    e.preventDefault();
+
+    if (!emailValid()) {
+      return false;
     }
-    return true;
-  }
-  // it's bad!
-  else {
-    $('input.email').classList.add('invalid');
-    $('input.email').focus();
-    $('.email-error').style.visibility = 'visible';
 
-    // so these don't get hooked repeatedly
-    $('input.email').removeEventListener('input', emailValid);
-    $('input.email').addEventListener('input', emailValid);
+    dom('button.submit').css({pointerEvents: 'none'});
+    dom('.signup').css({opacity: 0});
 
-    // if this is an event, we need to return true so the keypress is done
-    if (e)
-      return true;
+    fetch('/signup', {
+      method: 'POST',
+      body: new FormData($('form.signup-form'))
+    })
+    .then(function(response) {
+      if (response.status >= 200 && response.status < 300) {
+          dom('.thanks').css({display: 'block'});
+          dom('.form').css({display: 'none'});
+          dom('.signup').css({opacity: 1});
+      }
+      else {
+        log(response);
+        throw('signup error');
+      }
+    })
+    .catch(function(response) {
+      dom('button.submit').css({pointerEvents: ''});
+
+      if (response &&
+        response.responseJSON &&
+        response.responseJSON.messages) {
+          dom('.error').text(response.responseJSON.messages.join('. '))
+      }
+
+      dom('.error').css({display: block});
+      dom('.signup').css({opacity: 1});
+    });
 
     return false;
-  }
-};
-
-
-$('.signup-form').addEventListener('submit', function(e) {
-  e.preventDefault();
-
-  if (!emailValid()) {
-    return false;
-  }
-
-  $('button.submit').style.pointerEvents = 'none';
-  $('.signup').style.opacity = 0;
-
-  fetch("/signup", {
-    method: 'POST',
-    body: new FormData($('form.signup-form'))
-  }).then(function(response) {
-    if (response.status >= 200 && response.status < 300) {
-        $('.thanks').style.display = 'block';
-        $('.form').style.display = 'none'
-        $('.signup').style.opacity = 1;
-    }
-    else {
-      log(response);
-      throw('signup error');
-    }
-  })
-  .catch(function(response) {
-    $('button.submit').style.pointerEvents = '';
-
-    if (response &&
-      response.responseJSON &&
-      response.responseJSON.messages) {
-        $('.error').text(response.responseJSON.messages.join('. '))
-    }
-
-    $('.error').show();
-    $('.signup').css({opacity: 1});
   });
 
-  return false;
-});
 
+  function emailValid(e) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (re.test($('input.email').value)) {
+      // take off the invalid state
+      if (dom('input.email').is('.invalid')) {
+        dom('input.email').removeClass('invalid');
+        dom('.email-error').css({visibility: 'hidden'});
+      }
+      return true;
+    }
+    // it's bad!
+    else {
+      dom('input.email').addClass('invalid');
+      dom('.email-error').css({visibility: 'visible'});
+      
+      var input = dom.find(document, 'input.email')
+      
+      input.focus();
+
+      // so these don't get hooked repeatedly
+      input.removeEventListener('input', emailValid);
+      input.addEventListener('input', emailValid);
+
+      // if this is an event, we need to return true so the keypress is done
+      if (e)
+        return true;
+
+      return false;
+    }
+  };
+
+})();
