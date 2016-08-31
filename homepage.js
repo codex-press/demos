ab569668ff55ac39c60b39559bb708bded63034d
Tim Farnam <twfarnam@gmail.com>
2016-08-31 19:32:19 -0400

    fixes homepage for new version

--- homepage.js
+++ homepage.js
@@ -4,83 +4,61 @@
   var dom = require('dom').default;
   var log = require('log').default;
   var article = require('article').default;
+  var app = require('app').default;
 
   article.once('ready', function() {
+    var inputEl = dom.find('input.email');
 
-    dom('.signup-form').bind('submit', function(e) {
+    dom('.signup form').bind('submit', function(e) {
       e.preventDefault();
 
       if (!emailValid()) {
-        return false;
+        inputValidator();
+        inputEl.removeEventListener('input', inputValidator);
+        inputEl.addEventListener('input', inputValidator);
+        return;
       }
 
       dom('button.submit').css({pointerEvents: 'none'});
       dom('.signup').css({opacity: 0});
 
-      fetch('/signup', {
-        method: 'POST',
-        body: new FormData(dom('form.signup-form'))
-      })
-      .then(function(response) {
-        if (response.status >= 200 && response.status < 300) {
-            dom('.thanks').css({display: 'block'});
-            dom('.form').css({display: 'none'});
-            dom('.signup').css({opacity: 1});
-        }
-        else {
-          log(response);
-          throw('signup error');
-        }
-      })
-      .catch(function(response) {
-        dom('button.submit').css({pointerEvents: ''});
-
-        if (response &&
-          response.responseJSON &&
-          response.responseJSON.messages) {
-            dom('.error').text(response.responseJSON.messages.join('. '))
-        }
-
-        dom('.error').css({display: block});
-        dom('.signup').css({opacity: 1});
-      });
-
-      return false;
+      // submits to parent frame
+      app.send('signup', {email: dom.find('input.email').value});
     });
-  });
 
 
-  function emailValid(e) {
+    app.on('signupResponse', function(response) {
+      if (response.messages) {
+        dom('button.submit').css({pointerEvents: ''});
+        dom('.signup h2').text(response.messages.join('. '))
+      }
+      else {
+        dom('.signup h2').text('Thanks!');
+        dom('.signup form').css({display: 'none'});
+      }
+    });
+
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    var input = dom.find(document, 'input.email')
+    function emailValid() {
+      return re.test(inputEl.value);
+    };
 
-    if (re.test(input.value)) {
+    function inputValidator() {
+
       // take off the invalid state
-      if (dom('input.email').is('.invalid')) {
+      if (emailValid()) {
         dom('input.email').removeClass('invalid');
         dom('.email-error').css({visibility: 'hidden'});
       }
-      return true;
-    }
-    // it's bad!
-    else {
-      dom('input.email').addClass('invalid');
-      dom('.email-error').css({visibility: 'visible'});
-      
-      
-      input.focus();
+      // add invaild state
+      else {
+        dom('input.email').addClass('invalid');
+        dom('.email-error').css({visibility: 'visible'});
+        inputEl.focus();
+      }
+    };
 
-      // so these don't get hooked repeatedly
-      input.removeEventListener('input', emailValid);
-      input.addEventListener('input', emailValid);
-
-      // if this is an event, we need to return true so the keypress is done
-      if (e)
-        return true;
-
-      return false;
-    }
-  };
+  });
 
 })();
